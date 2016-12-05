a39e774f1f1f2856dfa0a2afe6b1c7ec88e10589
GRAMOFON\Michał <kapuchaster@gmail.com>
2016-12-05 13:40:44 +0100

    *Separated Login/Register screen (pseudo activity) *added a test child to parent communication by a callback function

--- index.android.js
+++ index.android.js
@@ -8,58 +8,32 @@ import React, { Component } from 'react';
 import WelcomeScreen from './welcomeScreen.js';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
   View,
-  Image,
-  TextInput,
-  Button,
-  Dimensions
+  Text,
 } from 'react-native';
 
-const screenWidth = Dimensions.get('window').width;
-
 export default class SnapVoter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+  screen: 'welcomeScreen'
+}
+  }
+
+  hand = (data) => {
+      this.setState({ screen: data });
+    }
+
   render() {
-    let logo = require('./img/Logo.jpg');
     return (
       <View>
-        <Image
-          style={{width: screenWidth, height: 110}}
-          source={logo}
-        />
-        <WelcomeScreen/>
-        <TextInput placeholder="Em@il"/>
-        <TextInput placeholder="Password"/>
-        <Button title="Sign In"/>
-
-        <TextInput placeholder="Em@il"/>
-        <TextInput placeholder="Full Name"/>
-        <TextInput placeholder="Username"/>
-        <TextInput placeholder="Password"/>
-        <Button title="Register"/>
+        <WelcomeScreen hand={this.hand} />
+        <Text> {this.state.screen} </Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
 
 AppRegistry.registerComponent('SnapVoter', () => SnapVoter);
--- welcomeScreen.js
+++ welcomeScreen.js
@@ -1,12 +1,102 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
 import React, { Component } from 'react';
 import {
-  Text
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  Dimensions
 } from 'react-native';
 
+const screenWidth = Dimensions.get('window').width;
+
 export default class WelcomeScreen extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
+    let logo = require('./img/Logo.jpg');
+
     return (
-      <Text> {"Welcome Screen"} </Text>
+      <View>
+        <Image
+          style={{width: screenWidth, height: 110}}
+          source={logo}
+        />
+        <TextInput placeholder="Em@il"/>
+        <TextInput placeholder="Password"/>
+        <Button title="Sign In" onPress={() => {this.props.hand('Success!')} }/>
+
+        <TextInput placeholder="Em@il"/>
+        <TextInput placeholder="Full Name"/>
+        <TextInput placeholder="Username"/>
+        <TextInput placeholder="Password"/>
+        <Button title="Register" onPress={postAcc}/>
+      </View>
     );
   }
 }
+
+function postAcc() {
+var http = new XMLHttpRequest();
+var url = "http://localhost:8082/register";
+var params = "login=a&password=b";
+http.open("POST", url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+ http.send(params);
+//   fetch('http://localhost:8082/register/', {
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       login: 'kk',
+//       password: 'pp',
+//     })
+//   }).then(function(response) {
+//   if(response.ok) {
+//
+//   }
+// }).then(function(data) {
+//
+// }).catch(function(error) {
+//   alert(error);
+// });
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
